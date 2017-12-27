3a58af360f76a5aadf2716b7f6c9b3e65136ad8e
chisl <info@chisl.io>
2017-12-27 18:11:08 +0100

    Initial commit

---
+++ BME680.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        BME680
+ * description: Low-power gas, pressure, temperature and humidity sensor
+ * manuf:       Bosch Sensortec
+ * version:     0.1
+ * url:         https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BME680-DS001-00.pdf
+ * date:        2017-12-18
+ * author       https://chisl.io/
+ * file:        BME680.cpp
+ */
+
+#include "BME680.hpp"
+
